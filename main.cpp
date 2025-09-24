caefb6fec096419c3e37b2256bc36995c3f43d8a
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-09-24 20:22:54 +0300

    resources added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,61 @@
 #include <windows.h>
+#include "resource.h"
+#include "spriteWindow.h"
 
-// Процедура обработки сообщений окна
+#define INACTIVITY_TIMER 1
+#define INACTIVITY_TIMEOUT 3000
+
+HINSTANCE currentInstance;
+
+INT_PTR CALLBACK AboutDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+        case WM_COMMAND:
+            if (LOWORD(wParam) == IDOK) {
+                EndDialog(hDlg, IDOK);
+                return TRUE;
+            }
+            break;
+        case WM_CLOSE:
+            EndDialog(hDlg, IDCANCEL);
+            return TRUE;
+    }
+    return FALSE;
+}
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    if (uMsg == WM_DESTROY) {
-        PostQuitMessage(0); // Завершение программы
-        return 0;
+    switch (uMsg) {
+        case WM_CREATE:
+            SetTimer(hwnd, INACTIVITY_TIMER, INACTIVITY_TIMEOUT, nullptr);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            HBRUSH hBrush = (HBRUSH)GetStockObject(WHITE_BRUSH);
+            FillRect(hdc, &ps.rcPaint, hBrush);
+            EndPaint(hwnd, &ps);
+            break;
+        }
+        case WM_TIMER:
+            KillTimer(hwnd, INACTIVITY_TIMER);
+            showSpriteWindow(currentInstance, hwnd);
+            break;
+        case WM_COMMAND:
+            switch (LOWORD(wParam)) {
+                case ID_HELP_ABOUT:
+                    DialogBox(currentInstance, MAKEINTRESOURCE(IDD_ABOUTBOX), hwnd, AboutDlgProc);
+                    break;
+            }
+            break;
+        case WM_MOUSEMOVE:
+        case WM_KEYDOWN:
+        case WM_LBUTTONDOWN:
+        case WM_RBUTTONDOWN:
+            KillTimer(hwnd, INACTIVITY_TIMER);
+            SetTimer(hwnd, INACTIVITY_TIMER, INACTIVITY_TIMEOUT, NULL);
+            break;
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
@@ -12,29 +63,33 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     const char CLASS_NAME[] = "SimpleWindowClass";
 
-    // Регистрация класса окна
-    WNDCLASS wc = {};
+    WNDCLASSEX wc = {};
+    wc.cbSize = sizeof(WNDCLASSEX);
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 
-    RegisterClass(&wc);
+    RegisterClassEx(&wc);
 
-    // Создание окна
-    HWND hwnd = CreateWindowEx(
-            0,                      // Расширенный стиль
-            CLASS_NAME,             // Имя класса
-            "Text editor",         // Заголовок окна
-            WS_OVERLAPPEDWINDOW,    // Стиль окна
-            CW_USEDEFAULT, CW_USEDEFAULT, 300, 200, // Размер и позиция
+    HWND mainWindow = CreateWindowEx(
+            0,
+            CLASS_NAME,
+            "Text editor",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT, 1000, 700,
             NULL, NULL, hInstance, NULL
     );
 
-    if (hwnd == NULL) return 0;
+    if (mainWindow == NULL) return 0;
 
-    ShowWindow(hwnd, nCmdShow);
+    currentInstance = hInstance;
+    HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MAINMENU));
+    SetMenu(mainWindow, hMenu);
+    ShowWindow(mainWindow, nCmdShow);
+    UpdateWindow(mainWindow);
 
-    // Цикл обработки сообщений
+
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
---
+++ resource.h
@@ -0,0 +1,9 @@
+#define IDR_MAINMENU     101
+#define ID_FILE_OPEN     102
+#define ID_FILE_SAVE     103
+#define ID_FILE_EXIT     104
+#define ID_EDIT_CUT      105
+#define ID_EDIT_COPY     106
+#define ID_EDIT_PASTE    107
+#define ID_HELP_ABOUT    108
+#define IDD_ABOUTBOX     201
